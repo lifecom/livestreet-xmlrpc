c56f428de86634bade5f54fb909633d021a1bbf6
esergeev777 <esergeev777@817e9c8c-ab69-11de-8ba5-91705c1537ed>
2009-09-27 13:50:40 +0000

    rev1

---
+++ classes/modules/xmlrpc/XmlRpc.class.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('XmlRpcServer.class.php');
+
+class LsXmlRpc extends Module {
+
+
+    public function Init() {
+        
+        
+    }
+
+
+    public function Test() {
+     /*       $login='admin';
+            $password = 'admin';
+            if (
+             (  // Пользователь может быть задан как логином, так и email-ом, поэтому нужно проверить и то и другое
+                (func_check($login,'mail') and $oUser=$this->User_GetUserByMail($login))  
+                or  $oUser=$this->User_GetUserByLogin($login)
+             )  and (
+                $oUser->getPassword()==func_encrypt($password) and $oUser->getActivate()
+             )
+        ) {
+                $oUser = $this->User_GetUserById($oUser->getId());
+
+                var_dump(!$oUser->isAdministrator());
+          }*/
+        new XmlRpcServer($this);
+    }
+}
